873ef601731159a7b44946996c75cb0801656bba
CsDenes <Denes@Denes-MacBook-Pro.local>
2016-04-07 20:16:36 +0200

    commmit

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
  * Created by Denes on 15/03/16.
  */
 
-console.log('Hello')
+
 var express = require('express');
 var app = express();
 
 app.use(express.static('static'));
 
 var server = app.listen(3000, function () {
+    console.log('Hello')
 });
